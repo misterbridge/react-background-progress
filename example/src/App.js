8b699b655de4afff8142518c90d874fd370b3a9f
Sylvain Pont <pont.sylvain@gmail.com>
2019-10-30 14:11:46 +0100

    Real component + Working exemple

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import ExampleComponent from 'react-background-progress';
+import ReactBackgroundProgress, {
+  DEFAULT_PROGRESS_ANIMATION_DURATION,
+} from 'react-background-progress';
 
-const App = () => <ExampleComponent text="Modern React component module" />;
+const App = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let updateTimeout;
+    if (progress < 1) {
+      updateTimeout = setTimeout(() => {
+        const newProgress = Math.round(Math.min(progress + 0.1, 1) * 10) / 10;
+        setProgress(newProgress);
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        alert('Done !');
+      }, DEFAULT_PROGRESS_ANIMATION_DURATION);
+    }
+    return () => {
+      clearTimeout(updateTimeout);
+    };
+  }, [progress]);
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <ReactBackgroundProgress progress={progress} />
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,71 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './styles.css';
 
-export default class ExampleComponent extends PureComponent {
-  static propTypes = {
-    text: PropTypes.string.isRequired,
-  };
+const DEFAULT_PROGRESS_ANIMATION_DURATION = 300;
 
-  render() {
-    const { text } = this.props;
-    return <div className={styles.test}>Example Component: {text}</div>;
-  }
+function getRotation(progress) {
+  return Math.min(180 * progress, 180);
 }
+
+const ReactBackgroundProgress = ({
+  progress,
+  animationDuration,
+  className,
+}) => {
+  const rotate = getRotation(progress);
+
+  const rootClasses = [styles.root, className];
+  // Hide if not active at
+  if (rotate !== 0) {
+    rootClasses.push(styles.visible);
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={rootClasses.join(' ')}>
+        <div className={styles.half}>
+          <div
+            className={[styles.fill, styles.rotate].join(' ')}
+            style={{
+              transform: `rotate(${rotate}deg)`,
+              transition: `transform ${animationDuration}ms linear`,
+            }}
+          />
+        </div>
+        <div
+          className={[styles.half, styles.rotate].join(' ')}
+          style={{
+            transform: `rotate(${rotate}deg)`,
+            transition: `transform ${animationDuration}ms linear`,
+          }}
+        >
+          <div
+            className={[styles.fill, styles.rotate].join(' ')}
+            style={{
+              transform: `rotate(${rotate}deg)`,
+              transition: `transform ${animationDuration}ms linear`,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactBackgroundProgress.propTypes = {
+  progress: PropTypes.number.isRequired, // 0 to 1
+  // Animation needs an additionnal X ms to reach progress value
+  animationDuration: PropTypes.number,
+  // Styles
+  className: PropTypes.string,
+};
+
+ReactBackgroundProgress.defaultProps = {
+  animationDuration: DEFAULT_PROGRESS_ANIMATION_DURATION,
+  className: '',
+};
+
+export default ReactBackgroundProgress;
+export { DEFAULT_PROGRESS_ANIMATION_DURATION };
